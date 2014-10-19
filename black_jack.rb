059def574fab3fe90754c7677a5bad269526fae7
katarina <golbang.k@gmail.com>
2014-10-19 22:54:54 +0200

    black jack game

--- black_jack.rb
+++ black_jack.rb
@@ -1,8 +1,11 @@
+require './player'
+require './dealer'
+
 class BlackJack
 
   module Interaction
     def say(text)
-      print text
+      puts text
     end
     def kick_off
       @players = []
@@ -11,17 +14,36 @@ class BlackJack
       num_of_players = gets.chomp
       if !num_of_players.chomp.match(/^[0-9]+$/).nil?
         num_of_players.to_i.times do |i|
-          say "Player ##{i}"
+          say "Player ##{i}:"
           @players << Player.new(gets.chomp)
         end
       else
-        say 'game terminated. The number of players not valid.'
+        say 'Game terminated. The number of players is not valid.'
       end
     end
   end
 
   module BusinessLogic
+    def next_player
+      @players.concat([@dealer])[@current_player+=1]
+    end
 
+    def play
+      @dealer = Dealer.new 'Jack'
+      @current_player = 0
+      @stack = CardStack.new
+      loop do
+        player = next_player
+        loop do
+          card = @dealer.give(@stack)
+        end
+      end
+    end
   end
+
   include Interaction
+
+
 end
+
+BlackJack.new.kick_off
--- giveable.rb
+++ giveable.rb
@@ -1,6 +1,7 @@
 module Giveable
-  def give(stack)
-    return nil if stack.empty?
-    stack.pop
+  def give(card_stack)
+    return nil if card_stack.nil?
+    return nil if card_stack.stack.empty?
+    card_stack.stack.pop
   end
 end
